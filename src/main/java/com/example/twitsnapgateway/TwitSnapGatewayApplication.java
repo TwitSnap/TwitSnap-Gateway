24c584dadbc7c9be805a9fdcf1cb7f76b9dcce99
Guido Menendez <guidomenendez0@gmail.com>
2024-09-21 16:55:11 -0300

    Start gateway

---
+++ src/main/java/com/example/twitsnapgateway/TwitSnapGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.example.twitsnapgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitSnapGatewayApplication{
+    public static void main(String[] args){
+        SpringApplication.run(TwitSnapGatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/twitsnapgateway/TwitSnapGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitsnapgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitSnapGatewayApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
