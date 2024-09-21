49fc59d53d51a321263dca537e46f1d9d066da99
Guido Menendez <guidomenendez0@gmail.com>
2024-09-21 17:12:13 -0300

    Add envVars

--- src/main/java/com/example/twitsnapgateway/TwitSnapGatewayApplication.java
+++ src/main/java/com/example/twitsnapgateway/TwitSnapGatewayApplication.java
@@ -1,11 +1,13 @@
 package com.example.twitsnapgateway;
 
+import com.example.twitsnapgateway.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TwitSnapGatewayApplication{
     public static void main(String[] args){
+        Config.setEnv();
         SpringApplication.run(TwitSnapGatewayApplication.class, args);
     }
 }
---
+++ src/main/java/com/example/twitsnapgateway/config/Config.java
@@ -0,0 +1,27 @@
+package com.example.twitsnapgateway.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Config {
+    /**
+     * Loads environment variables from the .env file and sets them
+     * as system properties.
+     */
+    public static void setEnv(){
+        Dotenv dotenv = Dotenv.load();
+
+        setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+        setProperty("LOG_PATH", dotenv.get("LOG_PATH"));
+        setProperty("LOG_LEVEL", dotenv.get("LOG_LEVEL"));
+    }
+
+    /**
+     * Sets a system property with the provided key and value.
+     *
+     * @param key   The key of the property to set.
+     * @param value The value of the property to set.
+     */
+    private static void setProperty(String key, String value){
+        System.setProperty(key, value);
+    }
+}
